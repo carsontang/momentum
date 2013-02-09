fd2e649e19637a4cf79ee23d511bfc37806015f0
Carson Tang <tang.carson@gmail.com>
2013-02-09 13:46:07 -0800

    AngularJS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Momentum::Application.config.secret_token = '93f0e02fc08951e2ad0c8344b8764a7e419a41180cca633aa566d62b7ab6521812e79bc6f4875f508da4a0d66527f513694409ad21f91cba488c45ea5f394de0'
